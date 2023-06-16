2ff65f7eaf17a94e00f5e926ddc9c2f2ff4f7a20
ParameswaruduAre <pavanabhi2117@gmail.com>
2023-06-16 23:32:58 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+
+app.get("/",function(req,res){
+    res.render("home")
+})
+
+
+
+
+
+
+
+app.listen(  process.env.PORT || 3000,function(req,res){
+    console.log("server is initiated in port 3000 sucessfully")
+})
