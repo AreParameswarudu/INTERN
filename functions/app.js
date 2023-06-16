d0f5925a55fe8101dbb17032d773a70566677176
ParameswaruduAre <pavanabhi2117@gmail.com>
2023-06-17 01:19:20 +0530

    first commit

--- functions/app.js
+++ functions/app.js
@@ -19,6 +19,6 @@ app.get("/",function(req,res){
 
 
 
-app.listen(  process.env.PORT || 3000,function(req,res){
+app.listen( 3000,function(req,res){
     console.log("server is initiated in port 3000 sucessfully")
 })
